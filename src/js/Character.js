2e3aba68fd696d83ec1b8196270385ccf06c86af
alexgithub <alexgithub@yandex.ru>
2024-02-22 15:39:33 +1000

    commit-3

--- src/js/Character.js
+++ src/js/Character.js
@@ -7,5 +7,34 @@ export default class Character {
         } else {
             throw new Error("10 >= name >= 2");
         }
+
+        if (type === 'Bowman'){
+            this.type = 'Bowman';
+            this.attack = 25;
+            this.defence = 25;
+        } else if (type === 'Swordsman') {
+            this.type = 'Swordsman';
+            this.attack = 40;
+            this.defence = 10;
+        } else if (type === 'Magician') {
+            this.type = 'Magician';
+            this.attack = 10;
+            this.defence = 40;
+        } else if (type === 'Undead') {
+            this.type = 'Undead';
+            this.attack = 25;
+            this.defence = 25;
+        } else if (type === 'Zombie') {
+            this.type = 'Zombie';
+            this.attack = 40;
+            this.defence = 10;
+        } else if (type === 'Daemon') {
+            this.type = 'Daemon';
+            this.attack = 10;
+            this.defence = 40;
+        } else {
+            throw new Error("one of the types: Bowman, Swordsman, Magician, Daemon, Undead, Zombie");
+            // const error = new Error("KRAH")
+        }
     }
 }
--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -1,17 +1,79 @@
 import Character from '../Character'
 
-test('проверяем: если символов в имени столько сколько нужно', () => {
-    const oleg = {name: 'Oleg', health: 100, level: 1};
-    const result = new Character('Oleg');
-    
-    expect(result).toEqual(oleg);
-});
+test('should myFunction: Bowman', () => {
+    const bowmanOleg = {name: 'Oleg', health: 100, level: 1, type: 'Bowman', attack: 25, defence: 25};
+    const result = new Character('Oleg', 'Bowman');
+    expect(result).toEqual(bowmanOleg);
+  });
 
-test('проверяем: если символов в имени больше или меньше', () => {
-    // const oleg = {name: 'Oleg', health: 100, level: 1};
-    // const result = new Character('Oleg');
-    
-    expect(() => new Character('O')).toThrowError("10 >= name >= 2");
-    expect(() => new Character('Olegggggggggggggggggggggggg')).toThrowError("10 >= name >= 2");
-});
+test('should myFunction: !Bowman', () => {
+    expect(() => new Character('')).toThrowError("10 >= name >= 2");
+  });
 
+
+test('should myFunction: Swordsman', () => {
+    const swordsmanOleg = {name: 'Oleg', health: 100, level: 1, type: 'Swordsman', attack: 40, defence: 10};
+    const result = new Character('Oleg', 'Swordsman');
+    expect(result).toEqual(swordsmanOleg);
+  });
+
+test('should myFunction: !Swordsman', () => {
+    expect(() => new Character('O', 'Swordsman')).toThrowError("10 >= name >= 2");
+    expect(() => new Character('Oleggggggggggggggg', 'Swordsman')).toThrowError("10 >= name >= 2");
+  });
+
+
+test('should myFunction: Magician', () => {
+    const magicianOleg = {name: 'Oleg', health: 100, level: 1, type: 'Magician', attack: 10, defence: 40};
+    const result = new Character('Oleg', 'Magician');
+    expect(result).toEqual(magicianOleg);
+  });
+
+test('should myFunction: !Magician', () => {
+    expect(() => new Character('O', 'Magician')).toThrowError("10 >= name >= 2");
+    expect(() => new Character('Oleggggggggggggggg', 'Magician')).toThrowError("10 >= name >= 2");
+  });
+
+
+  test('should myFunction: Undead', () => {
+    const undeadOleg = {name: 'Oleg', health: 100, level: 1, type: 'Undead', attack: 25, defence: 25};
+    const result = new Character('Oleg', 'Undead');
+    expect(result).toEqual(undeadOleg);
+  });
+
+test('should myFunction: !Undead', () => {
+    expect(() => new Character('O', 'Undead')).toThrowError("10 >= name >= 2");
+    expect(() => new Character('Oleggggggggggggggg', 'Undead')).toThrowError("10 >= name >= 2");
+  });
+
+
+  test('should myFunction: Zombie', () => {
+    const zombieOleg = {name: 'Oleg', health: 100, level: 1, type: 'Zombie', attack: 40, defence: 10};
+    const result = new Character('Oleg', 'Zombie');
+    expect(result).toEqual(zombieOleg);
+  });
+
+test('should myFunction: !Zombie', () => {
+    expect(() => new Character('O', 'Zombie')).toThrowError("10 >= name >= 2");
+    expect(() => new Character('Oleggggggggggggggg', 'Zombie')).toThrowError("10 >= name >= 2");
+  });
+
+
+test('should myFunction: Daemon', () => {
+    const daemonOleg = {name: 'Oleg', health: 100, level: 1, type: 'Daemon', attack: 10, defence: 40};
+    const result = new Character('Oleg', 'Daemon');
+    expect(result).toEqual(daemonOleg);
+  });
+
+test('should myFunction: !Daemon', () => {
+    expect(() => new Character('O', 'Daemon')).toThrowError("10 >= name >= 2");
+    expect(() => new Character('Oleggggggggggggggg', 'Daemon')).toThrowError("10 >= name >= 2");
+  });
+
+
+
+
+
+test('should myFunction: !Type', () => {
+    expect(() => new Character('Oleg', '')).toThrowError("one of the types: Bowman, Swordsman, Magician, Daemon, Undead, Zombie");
+  });
